e432f5454c63fdec2b7a89d8162d71ac375233f1
MoWahidAlbadawi <mohammadwahidalbadawi@gmail.com>
2024-11-10 00:38:12 +0300

    add resposive layout and navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Grid, GridItem } from "@chakra-ui/react"
+import NavBar from "./components/NavBar"
 
 function App() {
 
   return (
-    <>
-    <p>Hello Games Hub</p>
-    </>
+    <Grid templateRows={'reapeat(2,1fr)'} templateColumns={'reapet(2,1fr)'}>
+      <GridItem rowSpan={1} colSpan={2}>
+        <NavBar />
+      </GridItem>
+      <GridItem rowSpan={1} colSpan={1} bg='purple.500' display={{base : 'none' , lg : 'block'}}>aside</GridItem>
+      <GridItem rowSpan={1} colSpan={{base : 2 , lg : 1}} bg='green.500'>main</GridItem>
+    </Grid>
   )
 }
 
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+import { HStack, Image } from "@chakra-ui/react";
+import logo from '../assets/logo.webp'
+
+const NavBar = () => {
+    return <HStack>
+        <Image src={logo} height={'60px'}/>
+    </HStack>
+}
+export default NavBar;
