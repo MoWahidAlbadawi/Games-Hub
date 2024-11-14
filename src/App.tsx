035aa447f7b3da2243fe860ba19fa7a82d5d9661
MoWahidAlbadawi <mohammadwahidalbadawi@gmail.com>
2024-11-14 23:33:39 +0300

    add switch color mode

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import NavBar from "./components/NavBar"
 function App() {
 
   return (
+    <>
     <Grid templateRows={'reapeat(2,1fr)'} templateColumns={'reapet(2,1fr)'}>
       <GridItem rowSpan={1} colSpan={2}>
         <NavBar />
       </GridItem>
-      <GridItem rowSpan={1} colSpan={1} bg='purple.500' display={{base : 'none' , lg : 'block'}}>aside</GridItem>
-      <GridItem rowSpan={1} colSpan={{base : 2 , lg : 1}} bg='green.500'>main</GridItem>
+      <GridItem rowSpan={1} colSpan={1} display={{base : 'none' , lg : 'block'}}>aside</GridItem>
+      <GridItem rowSpan={1} colSpan={{base : 2 , lg : 1}} >main</GridItem>
     </Grid>
+    </>
   )
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,9 +1,11 @@
 import { HStack, Image } from "@chakra-ui/react";
 import logo from '../assets/logo.webp'
+import SwitchColorMode from "./SwitchColorMode";
 
 const NavBar = () => {
-    return <HStack>
+    return <HStack justifyContent={'space-between'} padding={'10px 20px'}>
         <Image src={logo} height={'60px'}/>
+        <SwitchColorMode />
     </HStack>
 }
 export default NavBar;
